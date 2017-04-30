b7f74e24ab39fd83acb5595961637468b706d4f4
Alex <alex_shu17@mail.ru>
2017-04-30 20:07:22 +0300

    added all files

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+/* ISOTOPE AND MASONRY */
+function useIsotope(event) {
+
+    // init Isotope
+    let isotopeGrid = new Isotope( '#portfolio-imgs-inner', {
+        itemSelector: '.item',
+        percentPosition: true,
+        masonry: {
+            // use element for option
+            columnWidth: '.item',
+            itemSelector: '.item',
+            transitionDuration: '1s',
+            gutter: 8,
+            isFitWidth: true,
+            horizontalOrder: true
+        }
+    });
+
+    let applyFilterFromLink = (linkObject) => {
+        let filterValue = linkObject.dataset.filter;
+
+        isotopeGrid.arrange({ filter: filterValue });
+    };
+
+    let filterGrid = function( event ) {
+        event.preventDefault();
+
+        applyFilterFromLink(this);
+
+        let activeBtn = document.querySelector('.filter-btn--active');
+        if (activeBtn) {
+            activeBtn.classList.remove('filter-btn--active');
+        }
+        this.classList.add('filter-btn--active');
+    };
+
+    document.querySelectorAll('.filter-btn').forEach(filterBtn => {
+        filterBtn.addEventListener( 'click', filterGrid);
+    });
+
+
+    let activeBtn = document.querySelector('.filter-btn--active');
+
+    applyFilterFromLink(activeBtn);
+}
+
+document.addEventListener('DOMContentLoaded', useIsotope);
+
+/* END ISOTOPE */
+
+
+
+/* SLICK SLIDER */
+$(document).ready(function(){
+    $('.team__carousel').slick({
+        arrows: false,
+        dots: true,
+    });
+});
+
+$(document).ready(function(){
+    $('.comments').slick({
+        arrows: false,
+        dots: true,
+    });
+});
+
+/* END SLICK SLIDER */
