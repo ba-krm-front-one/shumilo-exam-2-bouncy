37ed610532dbfc6cd3f2e7aaff1f3e952fd8daad
Alex <alex_shu17@mail.ru>
2017-05-02 17:28:37 +0300

    refactoring #1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 /* ISOTOPE AND MASONRY */
 function useIsotope(event) {
 
@@ -18,15 +17,12 @@ function useIsotope(event) {
 
     let applyFilterFromLink = (linkObject) => {
         let filterValue = linkObject.dataset.filter;
-
         isotopeGrid.arrange({ filter: filterValue });
     };
 
     let filterGrid = function( event ) {
         event.preventDefault();
-
         applyFilterFromLink(this);
-
         let activeBtn = document.querySelector('.filter-btn--active');
         if (activeBtn) {
             activeBtn.classList.remove('filter-btn--active');
@@ -38,17 +34,12 @@ function useIsotope(event) {
         filterBtn.addEventListener( 'click', filterGrid);
     });
 
-
     let activeBtn = document.querySelector('.filter-btn--active');
-
     applyFilterFromLink(activeBtn);
 }
-
 document.addEventListener('DOMContentLoaded', useIsotope);
 
 /* END ISOTOPE */
-
-
 
 /* SLICK SLIDER */
 $(document).ready(function(){
@@ -64,5 +55,42 @@ $(document).ready(function(){
         dots: true,
     });
 });
-
 /* END SLICK SLIDER */
+
+/* GOOGLE MAP */
+var map;
+function initMap() {
+    map = new google.maps.Map(document.querySelector('.contact-us__map'), {
+        center: {lat: 48.734454, lng: 37.579603},
+        zoom: 17,
+        disableDefaultUI: true
+    });
+
+    var image = 'imgs/beetroot-marker.png';
+    var marker = new google.maps.Marker({
+        position: {lat: 48.734404, lng: 37.579732},
+        map: map,
+        icon: image
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: 'We Are Here!'
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
+/* GOOGLE MAP END*/
+
+/* SMOOTH SCROLL */
+$(document).ready(function(){
+    $('a[href*=#]').bind("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 1000);
+        e.preventDefault();
+    });
+    return false;
+});
+/* SMOOTH SCROLL END*/
